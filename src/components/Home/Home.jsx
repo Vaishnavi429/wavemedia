340c80700cf6975468c542be32021e83c27c3cec
Rishabh Maurya <mauryarishabh559@gmail.com>
2022-05-14 21:13:19 +0530

    Add home page for showing all blog posts

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './Home.scss';
+import BlogPost from '../BlogPost/BlogPost';
+import { Container, Row, Col, Nav } from 'react-bootstrap';
+import data from '../blogData';
+import { Link, useRouteMatch } from 'react-router-dom';
+
+const Home = () => {
+    const blogData = data();
+
+    return (
+        <Container fluid>
+            <Row xs={1} md={2} lg={3} xl={3} xxl={4}>
+                {
+                    blogData.map((data, index) =>
+                        <Nav.Link as={Link} to={`${data.id}`} key={index} >
+                            <Col className='my-3' >
+                                <BlogPost
+                                    id={data.id}
+                                    postImageUrl={data.postImageUrl}
+                                    title={data.title}
+                                    description={data.description}
+                                    reads={data.reads}
+                                    time={data.time}
+                                    categories={data.categories}
+                                />
+                            </Col>
+                        </Nav.Link>
+                    )
+                }
+            </Row>
+        </Container>
+    )
+}
+
+export default Home;
