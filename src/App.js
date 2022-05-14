fb4f1b399b348109bf04f1d60d76f74be0d4f9ea
Rishabh Maurya <mauryarishabh559@gmail.com>
2022-05-14 21:17:29 +0530

    Add BlogPost, Home, ShowArticle components and dummy data blogData

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ActionPage from './components/ActionPage/ActionPage';
+import Home from './components/Home/Home';
+import ShowArticle from './components/ShowArticle/ShowArticle';
 
 const App = () => {
   return (
@@ -9,7 +10,8 @@ const App = () => {
       <div className="App">
         {/* try to make all function, arrow function */}
         <Routes>
-          <Route path='/' element={<ActionPage />} />
+          <Route path='/' element={<Home />} />
+          <Route path=':id' element={<ShowArticle />} />
         </Routes>
       </div>
     </BrowserRouter>
