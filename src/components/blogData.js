7ffb2f688deaf7af61c3e835136a5553c70aa770
Rishabh Maurya <mauryarishabh559@gmail.com>
2022-05-14 21:15:51 +0530

    Add dummy data for blog posts

---
+++ src/components/blogData.js
@@ -0,0 +1,71 @@
+const blogData = [
+    {
+        id : 'lizard-reptiles',
+        postImageUrl : 'https://cdn.pixabay.com/photo/2018/03/06/21/09/easter-3204589__340.jpg',
+        title : 'Rabbits are a widespread group of squamate reptiles, with over',
+        description : 'Rabbits are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica',
+        reads : 5,
+        time : 2,
+        categories : ['wild life', 'animal', 'nature']
+    },
+    {
+        id : 'sports-india',
+        postImageUrl : 'https://cdn.pixabay.com/photo/2014/11/07/00/00/volleyball-520093__340.jpg   ',
+        title : 'Athlete by nature, cheerleader by choice!',
+        description : "Sports can, through casual or organized participation, improve one's physical health. Hundreds of sports exist, from those between single contestants, through to those",
+        reads : 0,
+        time : 2,
+        categories : ['sports']
+    },
+    {
+        id : 'we-have-fun',
+        postImageUrl : 'https://cdn.pixabay.com/photo/2017/07/21/23/57/concert-2527495__340.jpg',
+        title : 'Claire Danes on The Essex Serpent: ‘I think the serpent is a symbol for people’s anxiety’',
+        description : 'Based on the hit novel by Sarah Perry, The Essex Serpent sees Claire Danes play Cora, who moves from London to the English county of Essex to look into the talk of a mythical serpent',
+        reads : 5,
+        time : 2,
+        categories : ['entertainment']
+    },
+    {
+        id : 'flowers-ideas',
+        postImageUrl : 'https://cdn.pixabay.com/photo/2012/09/08/21/51/flower-56414__340.jpg',
+        title : 'Flower Bouquet Names - 100 Ideas for Florists',
+        description : 'Coming up with unique and interesting bouquet & arrangement names is not easy! Here is a handy list for florist members of Direct2florist. Feel free to use these on your pages of Direct2florist or on your website provided by Direct2florist.',
+        reads : 5,
+        time : 2,
+        categories : ['nature', 'plants', 'flowers']
+    },
+    {
+        id : 'parenting-tips',
+        postImageUrl : 'https://cdn.pixabay.com/photo/2016/03/05/13/05/family-1237701__340.jpg',
+        title : 'Raise healthy, resilient kids with our research-based program',
+        description : 'Parenting Guide from Lasting empowers parents to become more confident in their approach to parenting. Through self-guided sessions and live classes, you’ll receive support on topics like anxiety in kids,',
+        reads : 100,
+        time : 1,
+        categories : ['parenting tips']
+    },
+    {
+        id : 'classic-music',
+        postImageUrl : 'https://cdn.pixabay.com/photo/2015/05/07/11/02/guitar-756326__340.jpg',
+        title : 'This article is about Western art music from the Middle Ages to the present.   ',
+        description : 'Classical music generally refers to the formal musical tradition of the Western world, considered to be distinct from Western folk music or popular music traditions.',
+        reads : 200,
+        time : 3,
+        categories : ['music']
+    },
+    {
+        id : 'singapur-based-startup',
+        postImageUrl : 'https://cdn.pixabay.com/photo/2017/11/30/09/03/business-2987962__340.jpg',
+        title : 'Singapore-based Good Startup raises $34M fund to invest in alternative protein companies',
+        description : 'Good Startup, a Singapore-based venture capital firm focused on alternative protein, has closed its latest fund. Consisting of $34 million, the new fund, called Good Protein Fund I, included partic...',
+        reads : 10,
+        time : 1,
+        categories : ['startups']
+    },
+];
+
+const data = () => {
+    return blogData;
+}
+
+export default data;
